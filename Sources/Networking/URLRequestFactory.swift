3e7c891e50721bd6a8ae120ce1a398b47b14e69b
Duy Tran <tphduy@gmail.com>
2020-09-18 23:02:00 +0700

    Implement `timeoutInterval: TimeInterval` for `URLRequestFactory`

--- Sources/Networking/URLRequestFactory.swift
+++ Sources/Networking/URLRequestFactory.swift
@@ -10,18 +10,22 @@ import Foundation
 public protocol URLRequestFactory {
     var host: String { get set }
     var cachePolicy: URLRequest.CachePolicy { get set }
+    var timeoutInterval: TimeInterval { get set }
     func make(endpoint: Endpoint) throws -> URLRequest
 }
 
 public struct DefaultURLRequestFactory: URLRequestFactory {
     public var host: String
     public var cachePolicy: URLRequest.CachePolicy
-
+    public var timeoutInterval: TimeInterval
+    
     public init(
         host: String,
-        cachePolicy: URLRequest.CachePolicy = .useProtocolCachePolicy) {
+        cachePolicy: URLRequest.CachePolicy = .useProtocolCachePolicy,
+        timeoutInterval: TimeInterval = 60) {
         self.host = host
         self.cachePolicy = cachePolicy
+        self.timeoutInterval = timeoutInterval
     }
 
     public func make(endpoint: Endpoint) throws -> URLRequest {
@@ -29,7 +33,10 @@ public struct DefaultURLRequestFactory: URLRequestFactory {
             throw NetworkingError.invalidURL(host)
         }
         url.appendPathComponent(endpoint.path)
-        var request = URLRequest(url: url)
+        var request = URLRequest(
+            url: url,
+            cachePolicy: cachePolicy,
+            timeoutInterval: timeoutInterval)
         request.allHTTPHeaderFields = endpoint.headers
         request.httpMethod = endpoint.method.rawValue.uppercased()
         request.httpBody = try endpoint.body()
