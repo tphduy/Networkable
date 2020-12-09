1e72622c5b2e95969e089290b10c2729fd2565ed
Duy Tran <tphduy@gmail.com>
2020-12-09 19:22:50 +0700

    Fulfill missing methods

--- Sources/Networkable/Method.swift
+++ Sources/Networkable/Method.swift
@@ -7,10 +7,33 @@
 
 import Foundation
 
+/// The object re-presents HTTP methods to indicate the desired action to be performed for a given resource
 public enum Method: String, Hashable, Equatable, CaseIterable {
+    
+    /// Requests a representation of the specified resource. Requests using GET should only retrieve data.
     case get
+    
+    /// Asks for a response identical to that of a GET request, but without the response body.
+    case head
+    
+    /// Submit an entity to the specified resource
     case post
+    
+    /// Replaces all current representations of the target resource with the request payload.
     case put
-    case patch
+    
+    /// Deletes the specified resource.
     case delete
+    
+    /// Establishes a tunnel to the server identified by the target resource.
+    case connect
+    
+    /// Describe the communication options for the target resource.
+    case options
+    
+    /// Performs a message loop-back test along the path to the target resource.
+    case trace
+    
+    /// Apply partial modifications to a resource.
+    case patch
 }
