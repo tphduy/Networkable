797789cbf096652d320df6dc132263de41badc7f
Duy Tran <tphduy@gmail.com>
2020-12-09 19:42:58 +0700

    Implement description for `Middleware`

--- Sources/Networkable/Middleware.swift
+++ Sources/Networkable/Middleware.swift
@@ -7,8 +7,20 @@
 
 import Foundation
 
+/// An object perform side effects wherever a request is sent or received.
 public protocol Middleware {
+    
+    /// Prepare a request before sending.
+    /// - Parameter request: the request to prepare.
     func prepare(request: URLRequest) throws -> URLRequest
+    
+    /// Notify a prepared request is about to be sent.
+    /// - Parameter request: the request will be sent.
     func willSend(request: URLRequest)
+    
+    /// Receive a response with data
+    /// - Parameters:
+    ///   - response: an object that provides response metadata.
+    ///   - data: the data returned by the server.
     func didReceive(response: URLResponse, data: Data) throws
 }
