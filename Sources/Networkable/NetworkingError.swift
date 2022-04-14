78007c650e8f03ae194dd5c9442c5a96f1ad7c5a
Duy Tran <duy.tran5@tiki.vn>
2022-04-14 18:02:00 +0700

    Enhance documentation of `NetworkableError`

--- Sources/Networkable/NetworkingError.swift
+++ Sources/Networkable/NetworkingError.swift
@@ -9,16 +9,15 @@ import Foundation
 
 /// The possible errors maybe  throw during sending a request and processing a response.
 public enum NetworkableError: Error, Equatable {
-    
     /// A response is well-received but without any data.
     case empty
     
-    /// The string re-presenting a request's URL is invalid
+    /// The text re-presents an URL of a request that is invalid.
     case invalidURL(String, relativeURL: URL?)
     
-    /// An unexpected response  is received.
+    /// An unexpected response is received.
     case unexpectedResponse(URLResponse)
     
-    /// A response has unacceptable status code is received.
+    /// A response with an unacceptable status code is received.
     case unacceptableStatusCode(ResponseStatusCode, URLResponse)
 }
