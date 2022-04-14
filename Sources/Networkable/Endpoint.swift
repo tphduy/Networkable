7823f3b06be25182b0c78ed294aecbafaf61bfb1
Duy Tran <duy.tran5@tiki.vn>
2022-04-14 17:57:28 +0700

    Enhance documentation of `Endpoint`

--- Sources/Networkable/Endpoint.swift
+++ Sources/Networkable/Endpoint.swift
@@ -9,17 +9,23 @@ import Foundation
 
 /// An object re-presents a HTTP request.
 public protocol Endpoint {
-    
-    /// A dictionary containing all of the header fields for the request.
+    /// A list of HTTP headers that let the client pass additional information with a request.
+    ///
+    /// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers).
     var headers: [String: String]? { get }
     
-    /// The relative url of the request.
+    /// A relative URL that identifies the location of a resource.
+    ///
+    /// https://developer.mozilla.org/en-US/docs/Learn/Common_questions/What_is_a_URL#absolute_urls_vs_relative_urls.
     var url: String { get }
     
-    /// The  method for the request.
+    /// An HTTP method indicate the desired action to be performed for a given resource.
+    ///
+    /// https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods.
     var method: Method { get }
     
     /// The data sent as the message body of the request.
+    ///
+    /// https://developer.mozilla.org/en-US/docs/Web/HTTP/Messages#body
     func body() throws -> Data?
 }
-
