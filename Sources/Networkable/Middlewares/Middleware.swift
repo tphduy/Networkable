aec7b89fccb8bb28b4f8fef4993db862f19aadfa
Duy Tran <duy.tran5@tiki.vn>
2022-04-14 19:47:08 +0700

    Fix typo

--- Sources/Networkable/Middlewares/Middleware.swift
+++ Sources/Networkable/Middlewares/Middleware.swift
@@ -10,16 +10,16 @@ import Foundation
 /// An object performs side effects whenever a request is sent or a response is received.
 public protocol Middleware {
     /// Prepare a request before sending.
-    /// - Parameter request: An object abstract information about a request.
+    /// - Parameter request: An object abstracts information about a request.
     func prepare(request: URLRequest) throws -> URLRequest
     
     /// Notify a prepared request is about to be sent.
-    /// - Parameter request: An object abstract information about the request.
+    /// - Parameter request: An object abstracts information about the request.
     func willSend(request: URLRequest)
     
     /// Receive a response with data
     /// - Parameters:
-    ///   - response: An object abstract information about a response.
+    ///   - response: An object abstracts information about a response.
     ///   - data: The data returned by the server.
     func didReceive(response: URLResponse, data: Data) throws
 }
