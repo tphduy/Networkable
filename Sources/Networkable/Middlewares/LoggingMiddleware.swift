cd0aadd3be738208add6de05acb9ab0283f577f3
Duy Tran <duy.tran5@tiki.vn>
2022-04-14 19:57:32 +0700

    Enhance `LoggingMiddleware`

--- Sources/Networkable/Middlewares/LoggingMiddleware.swift
+++ Sources/Networkable/Middlewares/LoggingMiddleware.swift
@@ -15,7 +15,7 @@ public struct LoggingMiddleware: Middleware {
     
     // MARK: - Dependencies
     
-    /// The log level to assign to the message
+    /// The log level to assign to the message.
     public var type: OSLogType
     
     /// The custom log object categorizes the log messages.
@@ -23,22 +23,21 @@ public struct LoggingMiddleware: Middleware {
 
     // MARK: - Init
     
-    /// Creates a middleware logs network activities to a logging system.
+    /// Initiate a middleware logs network activities to a logging system.
     /// - Parameters:
-    ///   - type: The log level to assign to the message. Default is `.default`.
-    ///   - log: The custom log object categorizes the log message. Default is `.default`.
+    ///   - type: The log level to assign to the message. The default value is `.default`.
+    ///   - log: The custom log object categorizes the log message. The default value is `.default`.
     public init(
         type: OSLogType = .default,
-        log: OSLog = .default) {
+        log: OSLog = .default
+    ) {
         self.type = type
         self.log = log
     }
     
     // MARK: - Middleware
     
-    public func prepare(request: URLRequest) throws -> URLRequest {
-        return request
-    }
+    public func prepare(request: URLRequest) throws -> URLRequest { request }
 
     public func willSend(request: URLRequest) {
         let message = log(request: request)
@@ -52,34 +51,28 @@ public struct LoggingMiddleware: Middleware {
 
     // MARK: - Main
     
-    /// Create a string describing a request.
-    /// - Parameter request: A request to describe.
-    /// - Returns: A string describes the request.
-    public func log(request: URLRequest) -> String {
-        return request.logging()
+    /// Return a `String` representing a request.
+    /// - Parameter request: An object abstracts information about the request.
+    /// - Returns:  a `String` representing a request.
+    func log(request: URLRequest) -> String {
+        request.logging()
     }
     
-    /// Create a string describing a response and its associated data that is loaded by a request before.
+    /// Return a `String` representing a response with the data returned by the server.
     /// - Parameters:
-    ///   - response: A response to describe.
-    ///   - data: A loaded data.
-    /// - Returns: A string describing a response and its associated data.
-    public func log(response: URLResponse, data: Data) -> String {
-        var message = response.logging()
-        
-        guard
-            !data.isEmpty,
-            let data = String(data: data, encoding: .utf8)
-        else { return message }
-        
-        message += "\n" + data
-        
-        return message
+    ///   - response: An object abstracts information about a response.
+    ///   - data: The data returned by the server.
+    /// - Returns: A `String` representing a response with the data returned by the server.
+    func log(response: URLResponse, data: Data) -> String {
+        [response.logging(), String(data: data, encoding: .utf8)]
+            .compactMap { $0 }
+            .filter { !$0.isEmpty }
+            .joined(separator: "\n")
     }
     
     /// Log a message to logging system.
     /// - Parameter message: A message to log.
-    public func log(message: String) {
+    func log(message: String) {
         if #available(iOS 10, macOS 10.12, macCatalyst 13.0, tvOS 10.0, watchOS 3.0, *) {
             os_log("%@", log: log, type: type, message)
         } else {
