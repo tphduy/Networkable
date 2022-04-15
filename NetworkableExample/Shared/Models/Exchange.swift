425c79eff1af6fc61b0e29094376bc3e83ee4c5d
Duy Tran <duy.tran5@tiki.vn>
2022-04-15 18:46:04 +0700

    Implement `Exchange.stubbed` for previewing

--- NetworkableExample/Shared/Models/Exchange.swift
+++ NetworkableExample/Shared/Models/Exchange.swift
@@ -10,13 +10,26 @@ import Foundation
 /// An object abstract an exchange is an organized market where (especially) tradable cryptocurrencies are bought and sold.
 struct Exchange: Codable, Equatable {
     let exchangeID, name, rank: String
-    let percentTotalVolume, volumeUsd: String?
-    let tradingPairs: String
     let exchangeURL: URL?
 
     enum CodingKeys: String, CodingKey {
         case exchangeID = "exchangeId"
-        case name, rank, percentTotalVolume, volumeUsd, tradingPairs
+        case name, rank
         case exchangeURL = "exchangeUrl"
     }
 }
+
+#if DEBUG
+extension Exchange {
+    /// A list of stubbed exchanges for previewing.
+    static var stubbed: [Exchange] {
+        let json = """
+        [{"exchangeId":"binance","name":"Binance","rank":"1","exchangeUrl":"https://www.binance.com/"},{"exchangeId":"hitbtc","name":"HitBTC","rank":"2","exchangeUrl":"https://hitbtc.com/"},{"exchangeId":"crypto","name":"Crypto.com Exchange","rank":"3","exchangeUrl":"https://api.crypto.com/"}]
+        """
+        let decoder = JSONDecoder()
+        return json
+            .data(using: .utf8)
+            .flatMap { try? decoder.decode([Exchange].self, from: $0) } ?? []
+    }
+}
+#endif
