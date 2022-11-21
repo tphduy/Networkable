dae4e602c7e04c48d74006714314f992da49a40c
Duy Tran <tphduy@gmail.com>
2022-11-21 20:55:06 +0700

    Updates the example projects with `AuthorizationMiddleware` usage

--- NetworkableExample/Shared/Extension/NetworkSession+CoinCap.swift
+++ NetworkableExample/Shared/Extension/NetworkSession+CoinCap.swift
@@ -14,11 +14,18 @@ extension NetworkSession {
     static var coincap: NetworkSession {
         let baseURL = URL(string: "https://api.coincap.io")
         let requestBuilder = URLRequestBuilder(baseURL: baseURL)
+        let authorization = AuthorizationMiddleware(
+            key: "Authorization",
+            value: "Bearer bc162a58-874e-47b7-828d-552cbbe1d31f",
+            place: .header)
+        let statusCode = StatusCodeValidationMiddleware()
+        let logging = LoggingMiddleware(type: .info, log: .network)
         let result = NetworkSession(
             requestBuilder: requestBuilder,
             middlewares: [
-                StatusCodeValidationMiddleware(),
-                LoggingMiddleware(type: .info, log: .network),
+                authorization,
+                statusCode,
+                logging,
             ])
         return result
     }
